e6d2a21eb9d4f07055a77c9fb23625ac1c71e0c9
Deepender Rathore <deepenrathore28@gmail.com>
2019-06-05 20:40:11 +0530

    skipping the testcase using runmode of the test data

--- src/test/java/com/deepak/testcases/AddCustomerTest.java
+++ src/test/java/com/deepak/testcases/AddCustomerTest.java
@@ -3,6 +3,7 @@ package com.deepak.testcases;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.deepak.base.TestBase;
@@ -10,8 +11,11 @@ import com.deepak.utilities.TestUtil;
 
 public class AddCustomerTest extends TestBase{
 	@Test(dataProviderClass = TestUtil.class,dataProvider="dp")
-	public void addCustomerTest(String firstname,String lastname,String postcode,String alertText) throws InterruptedException {
+	public void addCustomerTest(String runmode,String firstname,String lastname,String postcode,String alertText) throws InterruptedException {
 		
+		if (runmode.equalsIgnoreCase("N")) {
+			throw new SkipException("Skipping the test case as the runmode of the data is NO");
+		}
 		click("addCustBtn_CSS");
 		type("firstname_CSS", firstname);
 		type("lastname_XPATH",lastname);
